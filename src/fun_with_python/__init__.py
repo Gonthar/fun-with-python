7d043deaa62bb246f44a4436ac08b2958105e8a2
Maciej Gontar <maciej.gontar@gmail.com>
2020-11-07 12:48:54 +0100

    INIT commit, python project displaying random Wiki articles
    -- based on https://cjolowicz.github.io/posts/hypermodern-python-01-setup/
    -- added language selection feature

---
+++ src/fun_with_python/__init__.py
@@ -0,0 +1,2 @@
+# src/fun_with_python/__init__.py
+__version__ = "0.1.0"
---
+++ src/fun_with_python/console.py
@@ -0,0 +1,27 @@
+# src/fun_with_python/console.py
+import textwrap
+
+import click
+import locale
+import requests
+
+from . import __version__
+
+API_URL = "https://%s.wikipedia.org/api/rest_v1/page/random/summary"
+
+@click.command()
+@click.option("-l", "language", type=str,
+        default=locale.getlocale()[0].split("_")[0],
+        help="selected Wikipedia language")
+@click.version_option(version=__version__)
+def main(language):
+    """The hypermodern Python project."""
+    with requests.get(API_URL % language) as response:
+        response.raise_for_status()
+        data = response.json()
+
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="magenta")
+    click.echo(textwrap.fill(extract))
